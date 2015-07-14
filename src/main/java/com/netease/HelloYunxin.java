0d669e1023158344efb720285d408b70b1b84b7e
unknown <hzzhangmin2015@HIH-D-5807.hz.ntes.domain>
2015-07-14 20:49:28 +0800

    initial commit

---
+++ src/main/java/com/netease/HelloYunxin.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+package com.netease;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.github.kevinsawicki.http.HttpRequest;
+
+/**
+ * @author hzzhangmin2015
+ *
+ */
+public class HelloYunxin {
+	public static final String URL = 
+			"https://api.netease.im/nimserver/user/create.action";
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		RequestHeader rh = new RequestHeader();
+		Map<String, String> headers = new HashMap<String, String>();
+		headers.put("AppKey", RequestHeader.APP_KEY);
+		headers.put("Nonce", rh.getNonce());
+		headers.put("CurTime", rh.getCurTime());
+		headers.put("CheckSum", rh.genCheckSum());
+		
+		Map<String, String> data = new HashMap<String, String>();
+		data.put("accid", "user33");
+		data.put("name", "test333");
+		String response = HttpRequest.post(URL)
+				.headers(headers)
+				.contentType("application/x-www-form-urlencoded;charset=utf-8")
+				.form(data)
+				.body();
+		System.out.println(response);
+
+	}
+
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Hello Yunxin!</h2>
+</body>
+</html>
